cd44eacc09eda7045fd0aa77d8d8f74f7c44a5b2
Ian Gregg <iangregg188@gmail.com>
2020-07-30 13:48:55 -0700

    establishing new web application in the practice of C# & Databases.
    co-authored-by: Cody Fritz <clanalia55@gmail.com>

---
+++ BestRestaurants/StartUp.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AssNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NonProfit.Models;
+
+// best_restaurants |db|
+
+// |col| cuisine | cuisineId | foodtype | restaurantId
+
+// |col| restaurant | restaurantId | name | description | pricerange
